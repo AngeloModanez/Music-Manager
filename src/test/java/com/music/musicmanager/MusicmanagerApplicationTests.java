cf30208765387a8c90252fe9b1e2f08afbe4fef7
AngeloModanez <angelomodanez30@gmail.com>
2024-10-07 11:30:35 -0300

    :tada: init: Initialize Spring Project

---
+++ src/test/java/com/music/musicmanager/MusicmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.music.musicmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
